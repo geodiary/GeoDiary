2bbbab07df253227ec01f4e57429e9eb914ef9de
jtxtina <yanglq08@gmail.com>
2019-04-19 00:07:55 -0400

    Initial Commit

---
+++ GeoDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoDiary
+//
+//  Created by YangLingqin on 19/4/2019.
+//  Copyright © 2019 nyu.edu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
