05007ad5c0b6dcf78bda48aa4104044b8526518b
Sabah <sabahmsiddique@gmail.com>
2019-05-06 00:11:16 -0400

    added classes for handling map functionality

---
+++ GeoDiary/MapFunctions.swift
@@ -0,0 +1,13 @@
+//
+//  MapFunctions.swift
+//  GeoDiary
+//
+//  Created by Sabah Siddique on 5/6/19.
+//  Copyright © 2019 nyu.edu. All rights reserved.
+//
+
+import UIKit
+
+class MapFunctions: NSObject {
+
+}
