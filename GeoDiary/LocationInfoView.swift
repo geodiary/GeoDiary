efd3a810053c85bd75fb8e421305d0b5cbdcb450
Raihan Siddique <raihansiddique@Raihans-MacBook-Pro.local>
2019-05-10 14:25:20 -0400

    added LocationInfoView.swift and LocationInfo.xib

---
+++ GeoDiary/LocationInfoView.swift
@@ -0,0 +1,21 @@
+//
+//  LocationInfoView.swift
+//  GeoDiary
+//
+//  Created by Raihan Siddique on 5/10/19.
+//  Copyright © 2019 nyu.edu. All rights reserved.
+//
+
+import UIKit
+
+class LocationInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
